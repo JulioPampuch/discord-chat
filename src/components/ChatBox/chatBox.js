c0f33d7329e832b8d9cc5801ad5d093db4dd96ce
Julio Pampuch <juliopampuch@gmail.com>
2023-01-22 20:19:03 -0300

    added transparent background

--- src/components/ChatBox/chatBox.js
+++ src/components/ChatBox/chatBox.js
@@ -19,6 +19,13 @@ const StyledBackground = styled.section`
   flex-direction: column;
   justify-content: center;
 
+  background: rgba( 70, 70, 70, 0.95 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 2px );
+  -webkit-backdrop-filter: blur( 2px );
+  border-radius: 5px;
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
+
   margin: 32px 99px;
 `
 
